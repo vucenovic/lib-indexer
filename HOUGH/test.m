62d54c593fa0195a92d4c8617820c00de6eccd16
Anand Eichner <anand@ufo.at>
2019-10-16 22:13:18 +0200

    Improve detection and mess around with perspective transform

--- HOUGH/test.m
+++ HOUGH/test.m
@@ -1,31 +1,41 @@
 %{
-    Test for the hough transforms and edge detection
+    Test for the hough transforms, edge detection and perspective
+    correction
 %}
 
-function [] = testHough(fn,thres)
+function [] = testHough(fn,thres,peaks)
     baseImage = imread(fn);
-    baseImageGray = rgb2gray(baseImage);
-    edgeImage = edge(baseImageGray,"canny",[0.1,0.38]);
-    [H,T,R] = hough(edgeImage);
+    baseImageGray = rgb2gray(im2double(baseImage));
+    %edgeImage = edge(baseImageGray,"canny",[0.1,0.38]);
+    
+    %Only take horizontal Edges
+    %make sure to extend edges and not zero pad them to avoid detecting
+    %edges along the image borders
+    edgeImageHorizontal = imbinarize(abs(imfilter(baseImageGray,[1,2,1;0,0,0;-1,-2,-1],"replicate")));
+    edgeImageVertical = imbinarize(abs(imfilter(baseImageGray,[1,2,1;0,0,0;-1,-2,-1]',"replicate")));
 
+    %Transpose Image to reduce the houghspace to -20 to +20 degrees instead
+    %of having to use a full -90 to 89 degrees
+    [H,T,R] = hough(edgeImageHorizontal','RhoResolution',0.5,'Theta',-20:0.5:20);
+    
     imshow(H,[],'XData',T,'YData',R,'InitialMagnification','fit');
     xlabel('\theta'), ylabel('\rho');
     axis on, axis normal, hold on;
 
-    P  = houghpeaks(H,5,'threshold',ceil(thres*max(H(:))));
+    P  = houghpeaks(H,peaks,'threshold',ceil(thres*max(H(:))));
     x = T(P(:,2)); y = R(P(:,1));
     plot(x,y,'s','color','white');
 
-    lines = houghlines(edgeImage,T,R,P,'FillGap',5,'MinLength',7);
-    figure, imshow(edgeImage), hold on
+    lines = houghlines(edgeImageHorizontal',T,R,P,'FillGap',50,'MinLength',150);
+    figure, imshow(edgeImageHorizontal), hold on
     max_len = 0;
     for k = 1:length(lines)
        xy = [lines(k).point1; lines(k).point2];
-       plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+       plot(xy(:,2),xy(:,1),'LineWidth',2,'Color','green');
 
        % Plot beginnings and ends of lines
-       plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-       plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+       plot(xy(2,2),xy(2,1),'x','LineWidth',2,'Color','yellow');
+       plot(xy(1,2),xy(1,1),'x','LineWidth',2,'Color','red');
 
        % Determine the endpoints of the longest line segment
        len = norm(lines(k).point1 - lines(k).point2);
@@ -34,4 +44,16 @@ function [] = testHough(fn,thres)
           xy_long = xy;
        end
     end
+    
+    %why no work
+    %why does it explode into infinity once you start changing two axes
+    transformMatrix = fitgeotrans(...
+        [0 0; 0 1; 0.9 0.999; 0.9 0],...
+        [0 0; 0 1; 1 1; 1 0],...
+        "projective");
+    tranformImageOutputBounds=imref2d(size(baseImageGray),[1 size(baseImageGray,2)],[1 size(baseImageGray,1)]);
+    transformedImage = imwarp(baseImageGray,transformMatrix,"OutputView", tranformImageOutputBounds);
+    
+    figure();
+    imshow(transformedImage);
 end
