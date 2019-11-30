9dfe5ee36f68cb46a6e8912ded53910ab8c2c728
Anand Eichner <anand@ufo.at>
2019-11-30 01:49:53 +0100

    Slight refactoring of the PerspectiveCorrection

--- HOUGH/PerspectiveCorrection.m
+++ HOUGH/PerspectiveCorrection.m
@@ -1,19 +1,22 @@
 %{
     Attempts to correct the perspective of an Image based on the lines in it
+
+    Returns: The perspective corrected image and a Struct containing an
+            imRef2d object and the corners of the rectangle used to correct
+            the image.
+
     @Author Anand Eichner
 %}
-function [] = testHough(fn,thres,peakCount)
+function [correctedImage,spaceData] = PerspectiveCorrection(fn)
     close all;%%ffs
+    
+    thres = 0.4;
+    peakCount = 5;
+    VerticalAreaSelect = 20;
+
 
     baseImage = imread(fn);
     baseImageGray = rgb2gray(im2double(baseImage));
-    
-    linesA = [struct("P",[0,0],"D",[1,0]),struct("P",[0,1],"D",[1,0])];
-    linesB = [struct("P",[0,0],"D",[0,1]),struct("P",[1,0],"D",[0,1])];
-    a = findIntersections(linesA,linesB)
-    sortIntersections(a)
-    linesTest = [struct("P",[0,5],"D",[1,0]),struct("P",[0,1],"D",[1,0]),struct("P",[0,20],"D",[1,0]),struct("P",[0,2],"D",[1,0])];
-    selectLineCandidates(linesTest)
     
     %Only take horizontal Edges
     %make sure to extend edges and not zero pad them to avoid detecting
@@ -34,34 +37,40 @@ function [] = testHough(fn,thres,peakCount)
         )...
         );
     edgeImageVertical = bwareaopen(edgeImageVertical,500);
-    edgeImageVertical = bwareafilt(edgeImageVertical,peakCount);
+    edgeImageVertical = bwareafilt(edgeImageVertical,VerticalAreaSelect);
     
     %Transpose Image to reduce the houghspace to -20 to +20 degrees instead
     %of having to use a full -90 to 89 degrees
     [H,T,R] = hough(edgeImageHorizontal','RhoResolution',2,'Theta',-32.25:0.5:32);
+    P  = houghpeaks(H,peakCount,'threshold',ceil(thres*max(H(:))));
     
     %Removing the zero angle seems to improve the results quite significantly
     [H2,T2,R2] = hough(edgeImageVertical,'RhoResolution',2,'Theta',-32.25:0.5:32);
+    P2  = houghpeaks(H2,peakCount,'threshold',ceil(thres*max(H2(:))));
     
+    %{
     figure, imshow(H,[],'XData',T,'YData',R,'InitialMagnification','fit');
     xlabel('\theta'), ylabel('\rho');
     axis on, axis normal, hold on;
-
-    P  = houghpeaks(H,peakCount,'threshold',ceil(thres*max(H(:))));
+    
     x = T(P(:,2)); y = R(P(:,1));
     plot(x,y,'s','color','white');
+    %}
     
+    %{
     figure, imshow(H2,[],'XData',T2,'YData',R2,'InitialMagnification','fit');
     xlabel('\theta'), ylabel('\rho');
     axis on, axis normal, hold on;
     
-    P2  = houghpeaks(H2,peakCount,'threshold',ceil(thres*max(H2(:))));
     x2 = T2(P2(:,2)); y2 = R2(P2(:,1));
     plot(x2,y2,'s','color','white');
+    %}
+    
+    linesHorizontal = toLines(R,T,P,true);
+    linesVertical = toLines(R2,T2,P2,false);
     
     %%Plot horizontal line image
     figure, imshow(edgeImageHorizontal), hold on
-    linesHorizontal = toLines(R,T,P,true);
     for k = 1:length(linesHorizontal)
         line = linesHorizontal(k);
         xy = findIntersections([line],[struct("P",[0,0],"D",[0,1]),struct("P",[size(edgeImageHorizontal,2),0],"D",[0,1])]);
@@ -70,7 +79,6 @@ function [] = testHough(fn,thres,peakCount)
     
     %%Plot vertical line image
     figure, imshow(edgeImageVertical), hold on
-    linesVertical = toLines(R2,T2,P2,false);
     for k = 1:length(linesVertical)
         line = linesVertical(k);
         xy = findIntersections([line],[struct("P",[0,0],"D",[1,0]),struct("P",[0,size(edgeImageHorizontal,1)],"D",[1,0])]);
@@ -79,10 +87,10 @@ function [] = testHough(fn,thres,peakCount)
     
     linesHorizontal = selectLineCandidates(linesHorizontal);
     linesVertical = selectLineCandidates(linesVertical);
-    intersections = sortIntersections(findIntersections(linesHorizontal,linesVertical),true,[-1,0]);
-    figure; imshow(baseImage), hold on;
+    intersections = sortIntersections(findIntersections(linesVertical,linesHorizontal),true,[-1,0]);
     
-    for i = 0:size(intersections,1)-1
+    figure; imshow(baseImage), hold on;
+    for i = 0:size(intersections,1)-1;
         plot(intersections([i+1,mod(i+1,4)+1],1), intersections([i+1,mod(i+1,4)+1],2),"lineWidth", 4, "color", "red");
     end
     
@@ -92,8 +100,27 @@ function [] = testHough(fn,thres,peakCount)
         intersections,...
         [0 0; imageSize(2) 0; imageSize(2) imageSize(1); 0 imageSize(1)],...  %%kinda works [0 0; imageSize(1) 0; imageSize(1) imageSize(2); 0 imageSize(2)],...
         "projective");
-    transformedImage = imwarp(baseImageGray,transformMatrix);
-    figure(); imshow(transformedImage);
+    [correctedImage,spacialRef] = imwarp(baseImageGray,transformMatrix);
+    figure(); imshow(correctedImage);
+    
+    spaceData.imRef = spacialRef;
+    [x,y]=transformPointsForward(transformMatrix,intersections(:,1),intersections(:,2));
+    spaceData.houghBounds = [x,y];
+end
+
+%{
+    Takes: An array of lines
+
+    Returns: The distances of the lines to (0,0)
+
+    @Author Anand Eichner
+%}
+function [distances] = lineOriginDistances(lines)
+    norms = vertcat(lines.D);
+    norms = [norms(:,2),-norms(:,1)];
+    points = vertcat(lines.P);
+    
+    distances = abs(dot(norms,points,2));
 end
 
 %{
@@ -104,11 +131,7 @@ end
     @Author Anand Eichner
 %}
 function [lines] = selectLineCandidates(lines)
-    norms = vertcat(lines.D);
-    norms = [norms(:,2),-norms(:,1)];
-    points = vertcat(lines.P);
-    
-    dists = abs(dot(norms,points,2));
+    dists = lineOriginDistances(lines);
     dists = [dists,(1:length(dists))'];
     dists = sortrows(dists,1);
     
@@ -203,5 +226,5 @@ function [intersections] = sortIntersections(intersections,reverse,zeroVector)
     %%Sort by angles
     intersections = [intersections,angle];
     intersections = sortrows(intersections,3,direction);
-    intersections = intersections(:,1:2)
+    intersections = intersections(:,1:2);
 end
