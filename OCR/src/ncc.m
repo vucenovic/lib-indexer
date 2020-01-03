a6f181240323cbaad4b2a99c5f975128bd847280
Aleksandar Vucenovic <aleks.vucenovic@gmail.com>
2020-01-03 03:05:35 +0100

    major changes to ncorr algorithm, using right ncorr formular, unpadding needed

--- OCR/src/ncc.m
+++ OCR/src/ncc.m
@@ -7,7 +7,7 @@
 % value between -1 and 1, wheras 1 means they fully correlate, -1 means it
 % correlates with its negation, 0 means no correlation.
 
-% This code follows the following paper:
+% NCC description:
 % http://scribblethink.org/Work/nvisionInterface/nip.pdf
 
 % The correlation coefficient that must be reached at its peak is 0.5,
@@ -21,7 +21,6 @@
 function coefficient = ncc(template, img)
 template = imcomplement(imread('temp/L.bmp'));
 img = imread('temp/tempSubImage11.png');
-
 % check prerequisites
 
 [x, y, z] = size(img); 
@@ -46,47 +45,50 @@ correlation = [];
 sizeTemplate = size(template);
 sizeImage = size(img);
 
-% add padding to original image
-
-padded = padarray(img, sizeTemplate);
-
 % calculate local sum (hinted by:
 % https://blogs.mathworks.com/steve/2006/05/02/fast-local-sums/)
 
-localsum = localSum(padded, sizeTemplate);
+localsum = localSum(img, sizeTemplate);
 
-dotImg = img.*img;
-padded = padarray(dotImg, sizeTemplate);
-quadsum = localSum(padded, sizeTemplate);
+quadImg = img.^2;
+quadsum = localSum(quadImg, sizeTemplate);
 
-% calculate standard deviation
+% calculate standard deviation and mean
 
-stdDev = quadsum - localsum.^2;
-stdDev = stdDev/numel(template);
-stdDev = sqrt(max(0, stdDev));
+diff = quadsum - localsum.^2/numel(template);
+sigmaImg = sqrt(max(0, diff));
+stdTemplate = std(template(:));
+meanTemplate = mean(mean(template));
 
-stdDevTemplate = std(template(:)) * sqrt(numel(template) - 1);
+if stdTemplate == 0
+    error("the values of the template must not be the same!")
+end
 
-% calculate mean
-
-mean = sum(template(:))/numel(template) * localsum;
+sigmaTemplate = sqrt(numel(template) - 1) * stdTemplate;
+denom = sigmaTemplate * sigmaImg;
 
 % calculate correlation using fast fourier transform
 
 corr = fftCorr(template, img, sizeImage, sizeTemplate);
+numer = (corr - localsum * meanTemplate);
 
-% calculate ncc
+% calculate ncorr
 
-i = max(stdDev, stdDevTemplate/1e5);
-ncc = 0.5+(corr - mean)./(stdDevTemplate * i * 2);
-correlation = ncc;
+correlation = numer/denom;
+
+% set values outside of -1 and 1 to zero (happens through variance)
+
 
 end
 
 function localsum = localSum(img, sizeTemplate)
-% calculate local sum (hinted by:
-% https://blogs.mathworks.com/steve/2006/05/02/fast-local-sums/)
-% author: aleksandar vucenovic
+% calculates the correlation between template
+% and image by using fast fourier transform
+% author:  Dirk-Jan Kroon (2020). Fast/Robust Template Matching
+%(https://www.mathworks.com/matlabcentral/fileexchange/24925-fast-robust-template-matching),
+% MATLAB Central File Exchange. Retrieved January 3, 2020. 
+
+% note: minor changes to author's code for readability
 
 cumulative = cumsum(img,1);
 dim1 = cumulative(1 + sizeTemplate(1):end-1,:);
@@ -99,17 +101,28 @@ localsum = dim1 - dim2;
 end
 
 function corr = fftCorr(template, Image, sizeImage, sizeTemplate)
-% calculates the correlation between template
-% and image by using fast fourier transform
 % author: aleksandar vucenovic
+
+% note: followed this paper 
+% http://scribblethink.org/Work/nvisionInterface/nip.pdf
 
 flip180 = rot90(template,2);
 totalSize = sizeImage + sizeTemplate - 1;
-fourierTemplate = fft2(flip180, totalSize(1), totalSize(2));
-fourierImage = fft2(Image, totalSize(1), totalSize(2));
-corr = ifft2(fourierTemplate.*fourierImage);
+fourierTemplate = fft(flip180, totalSize(1), totalSize(2));
+fourierImage = fft(Image, totalSize(1), totalSize(2));
+corr = ifft(fourierTemplate.*fourierImage);
 corr = real(corr);
 
+end
+
+function result = forceSize(mat, target)
+one = target;
+
+for k = 1:2
+    if size(mat,k) > target[k]
+        difference = (size(mat,k) - target[k])/2;
+        one = [floor:
+end
 end
 
 
