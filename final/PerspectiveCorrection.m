ccd80b62b5fb1ca72e3cc7115f1e47762d3f6a51
Anand Eichner <anand@ufo.at>
2020-01-05 16:23:01 +0100

    stuff

--- final/PerspectiveCorrection.m
+++ final/PerspectiveCorrection.m
@@ -7,7 +7,7 @@
             imRef2d object, the corners of the rectangle used to correct
             the image, and all horizontal Lines that were used(found).
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [correctedImage,spaceData] = PerspectiveCorrection(baseImage)
     baseImageGray = rgb2gray(im2double(baseImage));
@@ -94,6 +94,7 @@ function [correctedImage,spaceData] = PerspectiveCorrection(baseImage)
     
     %% Write additional return data
     spaceData.imRef = spacialRef;
+    spaceData.transform = transformMatrix;
     imOffset = [-spacialRef.XWorldLimits(1),-spacialRef.YWorldLimits(1)];
     spaceData.originalBounds = [0 0; imageSize(2) 0; imageSize(2) imageSize(1); 0 imageSize(1)] + repmat(imOffset,4,1);
     spaceData.horizontals = transformLines(linesHorizontal,transformMatrix,imOffset);
@@ -150,7 +151,7 @@ end
 
     Returns: The distances of the lines to (0,0) (signed depending on direction)
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [distances] = lineOriginDistances(lines)
     norms = vertcat(lines.D);
@@ -165,7 +166,7 @@ end
 
     Returns: Two best line candidates for generating a perspective plane
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [lines] = selectLineCandidates(lines)
     dists = lineOriginDistances(lines);
@@ -180,7 +181,7 @@ end
 
     Returns: An array of Lines
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [lines] = toLines(R,T,P,transposed)
     a = -T(P(:,2));
@@ -200,7 +201,7 @@ end
 
     Returns: An array of all Intersections between the two groups
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [intersections] = findIntersections(lineGroupA,lineGroupB)
     intersections = [];
@@ -217,7 +218,7 @@ end
     Returns: The intersection Point
         (an empty Vector if coliniear or parallel)
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [intersection,t] = findIntersection(lineA,lineB)
     d=lineA.D(1)*lineB.D(2)-lineA.D(2)*lineB.D(1);
@@ -233,7 +234,7 @@ end
 
     Returns: A sorted list of the intersections
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [intersections] = sortIntersections(intersections,reverse,zeroVector)
     %% Process arguments
@@ -296,7 +297,7 @@ end
 
     Returns: The two points of intersection
 
-    @Author Anand Eichner
+    Author:         Anand Eichner (11808244)
 %}
 function [intersections] = intersectBox(line,box)
     intersections = [];
