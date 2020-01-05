4181ea3288b34270602f71df0b4ba6e297c36ba7
Anand Eichner <anand@ufo.at>
2020-01-05 17:12:16 +0100

    fix fucky jpg shit

--- final/main.m
+++ final/main.m
@@ -3,6 +3,33 @@
 %}
 function [] = main(imagePath)
     image = imread(imagePath);
+    %% Fix fucky jpg standard bullshit
+    info = imfinfo(imagePath);
+    if isfield(info,'Format') && info(1).Format == "jpg" && isfield(info,'Orientation')
+       orient = info(1).Orientation;
+       switch orient
+         case 1
+            %normal, leave the data alone
+         case 2
+            image = image(:,end:-1:1,:);         %right to left
+         case 3
+            image = image(end:-1:1,end:-1:1,:);  %180 degree rotation
+         case 4
+            image = image(end:-1:1,:,:);         %bottom to top
+         case 5
+            image = permute(image, [2 1 3]);     %counterclockwise and upside down
+         case 6
+            image = rot90(image,3);              %undo 90 degree by rotating 270
+         case 7
+            image = rot90(image(end:-1:1,:,:));  %undo counterclockwise and left/right
+         case 8
+            image = rot90(image);                %undo 270 rotation by rotating 90
+         otherwise
+            warning(sprintf('unknown orientation %g ignored\n', orient));
+       end
+    end
+    
+    %% actual stuff
     [image, distortionData] = PerspectiveCorrection(image);
     
     shelf_height_px = calc_shelf_height(distortionData);
